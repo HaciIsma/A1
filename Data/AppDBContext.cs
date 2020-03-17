e4b086e4137194785ea3de4702e49a9c7928285d
HaciIsma <haciismayilov5@outlook.com>
2020-03-17 08:15:37 +0400

    3 commit

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using A1.Model;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1.Data
+{
+    class AppDBContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = "Users.db" };
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+
+            optionsBuilder.UseSqlite(connection);
+        }
+    }
+}
---
+++ Service/DBService.cs
@@ -0,0 +1,47 @@
+﻿using A1.Data;
+using A1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1.Service
+{
+    public class DBService
+    {
+        static DBService()
+        {
+            using (AppDBContext appDB = new AppDBContext())
+            {
+                appDB.Database.EnsureCreated();
+            }
+        }
+
+        public void Add(User user)
+        {
+            using (AppDBContext appDB=new AppDBContext())
+            {
+                appDB.Add(user);
+                appDB.SaveChanges();
+            }
+        }
+
+        public User Search(Expression<Func<User, bool>> filter)
+        {
+            using (AppDBContext appDB = new AppDBContext())
+            {
+                return appDB.Users.FirstOrDefault(filter);
+            }
+        }
+
+        public void Delete(Expression<Func<User,bool>> filter) 
+        {
+            using (AppDBContext appDB = new AppDBContext())
+            {
+                appDB.Users.Remove(appDB.Users.FirstOrDefault(filter));
+            }
+        }
+    }
+}
